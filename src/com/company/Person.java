0571d754c38d8aed182966f02db096ad1efdbb40
MandeepSandhu1990 <mannugoraya1990@gmail.com>
2022-03-04 22:08:08 -0500

    notes one

---
+++ src/com/company/Person.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String address;
+    private int age;
+
+//public person(){
+//    System.out.println("Constructor is called");
+//     this.firstname = ;
+//     this.lastname;
+//     this.address;
+//     this.age;
+//
+//}
+
+//    public void changeAddress(String address){
+//        this.address = address;
+//    }
+//
+//    public String getFullName() {
+//        return firstName + lastName;
+//    }
+//
+//    public String getAddress(){
+//        return this.address;
+//    }
+//
+//
+//
+}
+
